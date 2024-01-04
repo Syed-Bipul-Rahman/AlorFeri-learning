ccb891de6a5683430cf41d0fc7f40dbaf938b38a
Syed Bipul Rahman <123935352+Syed-Bipul-Rahman@users.noreply.github.com>
2024-01-04 11:27:11 +0600

    Create main.kt

---
+++ main.kt
@@ -0,0 +1,63 @@
+//fun sayhello(name: String)=println("Hello $name")
+
+// fun sayhello(greet: String, lang: List<String>) {
+//   lang.forEach { lang ->
+//       println("$greet $lang")
+//   }
+// }  
+
+fun main() {
+
+//var datas=arrayOf("Kotlin","Java","Python","C++","C#")
+//var datas=listOf("Kotlin","Java","Python","C++","C#")
+// println(datas[0])
+// println(datas.get(1))
+//println("the size of data is ${datas.size}")
+
+
+//for loop
+
+// for (data in datas){
+//     println(data)
+  
+//   }
+
+//foreach
+
+// datas.forEach { 
+//     println(it)}
+
+
+// datas.forEachIndexed { index, data ->
+//     println("$index : $data")
+
+// }
+
+
+// val map= mapOf(1 to "Kotlin",2 to "Java",3 to "Python",4 to "C++",5 to "C#")
+
+
+// map.forEach { (key, value) -> println("$key : $value") }
+
+
+//val lang=listOf( "Kotlin", "Java", "Python", "C++", "C#")
+
+//map.forEach { (key, value) -> println("$key -> $value") }
+
+// sayhello("Hello",lang)
+
+//sayhello(lang)
+
+// val lang = listOf("Kotlin", "Java", "Python", "C++", "C#")
+
+//     sayhello("Hello", lang)
+
+
+
+//instance of person
+val person=Person("John","Doe")
+println(person.firstName)
+
+
+
+}
